bafd930734761cefeb06aef1cfe0ac64b76371b7
Berker OĞUZ <tberkeroguz@gmail.com>
2023-05-27 16:21:31 +0300

    ilk commit attigimi gosteriyorum

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Alttaki kod sisteme "berker sümeyyeyi seviyor" yazar.
+        /*
+        Paragraf Yorum Satırı
+         Paragraf Yorum Satırı Paragraf Yorum Satırı
+          Paragraf Yorum Satırı
+           Paragraf Yorum Satırı
+
+         */
+        // Java büyük harf küçük harfe duyarlı bir dildir. camelCase olayı.
+        System.out.println("Berker Sümeyye'yi seviyor.");  // System class'ından > onun içindeki out değişkeni > println methodunu çağırıyor.
+
+        // Kısayolu >> sout
+        System.out.println("Kısa yoldan sout yazarak system.out.println yazıldı.");
+
+        // ------------ Veri Tipleri ---------------
+        //
+        int a = 10;
+        int b = 20;
+        System.out.println(" Yukarıdaki a sayısının değeri =  " + a );
+        System.out.println(" Yukarıdaki a ve b sayılarının toplamı = " + a+ " " +b );
+        System.out.println("Yukarıdaki a ve sayılarının toplamı = "+ (a+b));
+
+        String isim = "Sümeyye";
+        String isim2 = "Berker";
+
+        // Sout içerisinde "Sümeyye Berker'i seviyor" yazdıralım
+
+        System.out.println(isim+isim2+"i seviyor"); //SümeyyeBerkeri seviyor
+        System.out.println(isim + " " + isim2+"'i seviyor"); // Sümeyye Berker'i seviyor
+
+    }
+
+}
---
+++ src/OrnekSoru1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class OrnekSoru1 {
+
+    public static void main(String[] args) {
+
+        /*
+           Kullanıcıdan Yaşını isteyiniz. Girilen yaşa göre emekli olabilirsin veya olamazsın yazdırınız.
+
+         Emekli olma şartı 65 ve üzeri yaş gerektirir.
+         65ten küçükse emekli olamazsın yazacak.
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen yaşınızı giriniz");
+        int yas = scan.nextInt();
+
+        if (yas < 30 || yas > 120) {
+            // Yas 30dan küçük ya da 120den büyük olamaz
+            System.out.println("Lütfen düzgün bir yaş giriniz. ");
+        } else {
+
+            if (yas >= 65) {
+                System.out.println("Emekli olabilirsin");
+            } else System.out.println("Emekli olamazsınız!");
+
+        }
+
+
+    }
+}

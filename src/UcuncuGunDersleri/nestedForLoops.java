186c6dd64dd52e094c707d313db20d571d0b5cb6
Berker OĞUZ <tberkeroguz@gmail.com>
2023-06-04 15:53:23 +0300

    3.gun dersleri

---
+++ src/UcuncuGunDersleri/nestedForLoops.java
@@ -0,0 +1,22 @@
+package UcuncuGunDersleri;
+
+public class nestedForLoops {
+    public static void main(String[] args) {
+
+        // Bunun döngü sayısını kullanıcıdan alarak , işlem yapan halini yazınız.
+
+        for ( int i = 1; i<=5; i++){ // her satır için işlem yapma olanağı sağlar
+
+            for (int a=0; a<i;a++){ // her satırda yukarıdaki i'ye kadar döngü sağlar.
+                System.out.print("*");
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
+
+    }
+
